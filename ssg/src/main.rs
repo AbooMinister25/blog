c7f61444f4c659ccba9a165eec5b5bc955e3e4e8
AbooMinister25 <aboominister@gmail.com>
2022-09-14 19:24:38 -0400

    Remove lib.rs, move all content to main.rs

--- ssg/src/main.rs
+++ ssg/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+#![warn(clippy::pedantic, clippy::nursery)]
+#![allow(clippy::must_use_candidate)]
+#![allow(clippy::unnecessary_wraps)]
+#![allow(clippy::missing_const_for_fn)]
+#![allow(clippy::return_self_not_must_use)]
+#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::missing_panics_doc)]
+
+pub mod markdown;
+pub mod models;
+pub mod schema;
+
+use color_eyre::eyre::Result;
+use rusqlite::Connection;
+
+fn setup() -> Result<()> {
+    let conn = Connection::open("blog.db")?;
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS posts (
+            id INTEGER PRIMARY KEY,
+            title VARCHAR NOT NULL,
+            content TEXT NOT NULL,
+            summary TEXT NOT NULL,
+            tags TEXT NOT NULL,
+            published BOOLEAN NOT NULL DEFAULT 'f',
+            published_at TIMESTAMP NOT NULL
+        )",
+        (),
+    )?;
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    setup()?;
+    Ok(())
 }
