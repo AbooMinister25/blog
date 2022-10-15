ca37d43a3a5574ae67c37402561c221cb0aee884
AbooMinister25 <aboominister@gmail.com>
2022-10-15 16:48:01 -0400

    Changed server from Axum to Rocket

--- server/src/endpoints.rs
+++ server/src/endpoints.rs
@@ -1,14 +1,9 @@
-use axum::{http::StatusCode, response::Html};
+use rocket::response::content::RawHtml;
 use std::include_str;
 
 static INDEX_CONTENT: &str = include_str!("../../public/index.html");
 
-// Health check endpoint
-pub async fn health_check() -> StatusCode {
-    StatusCode::OK
-}
-
-// root endpoint
-pub async fn index() -> Html<&'static str> {
-    Html(INDEX_CONTENT)
+#[get("/")]
+pub async fn index() -> RawHtml<&'static str> {
+    RawHtml(INDEX_CONTENT)
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,36 +1,15 @@
+#[macro_use]
+extern crate rocket;
+
 mod endpoints;
 
-use axum::{
-    http::StatusCode,
-    response::IntoResponse,
-    routing::{get, get_service},
-    Router,
-};
-use axum_extra::routing::SpaRouter;
-use endpoints::{health_check, index};
-use std::io;
-use tower_http::services::ServeDir;
+use crate::endpoints::index;
+use rocket::fs::FileServer;
 
-#[tokio::main]
-async fn main() {
-    let serve_static = get_service(ServeDir::new("./static")).handle_error(handle_error);
-    let styles_router = SpaRouter::new("/styles", "./styles");
-
-    let app = Router::new()
-        .route("/", get(index))
-        .route("/health-check", get(health_check))
-        .merge(styles_router)
-        .nest("/static", serve_static);
-
-    tracing_subscriber::fmt::init();
-    tracing::info!("Listening on http://0.0.0.0:3000");
-
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-}
-
-async fn handle_error(_err: io::Error) -> impl IntoResponse {
-    (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong...")
+#[rocket::launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/styles", FileServer::from("./styles"))
+        .mount("/static", FileServer::from("./static"))
 }
