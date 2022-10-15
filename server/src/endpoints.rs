6dea9fadb9805714ee8ac83e3764041d7a33552f
AbooMinister25 <aboominister@gmail.com>
2022-10-15 16:59:24 -0400

    Add support for serving posts in server

--- server/src/endpoints.rs
+++ server/src/endpoints.rs
@@ -1,9 +1,17 @@
-use rocket::response::content::RawHtml;
+use rocket::{fs::NamedFile, response::content::RawHtml};
 use std::include_str;
+use std::path::Path;
 
 static INDEX_CONTENT: &str = include_str!("../../public/index.html");
 
 #[get("/")]
-pub async fn index() -> RawHtml<&'static str> {
-    RawHtml(INDEX_CONTENT)
+pub async fn index() -> Option<NamedFile> {
+    NamedFile::open(Path::new("./public/index.html")).await.ok()
+}
+
+#[get("/posts/<title>")]
+pub async fn get_post(title: &str) -> Option<NamedFile> {
+    NamedFile::open(Path::new("./public/").join(format!("{title}.html")))
+        .await
+        .ok()
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -3,13 +3,13 @@ extern crate rocket;
 
 mod endpoints;
 
-use crate::endpoints::index;
+use crate::endpoints::{get_post, index};
 use rocket::fs::FileServer;
 
 #[rocket::launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index])
+        .mount("/", routes![index, get_post])
         .mount("/styles", FileServer::from("./styles"))
         .mount("/static", FileServer::from("./static"))
 }
