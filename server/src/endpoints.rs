c04d9cc576681a5d2d6f1fcae5416886c4a101be
AbooMinister25 <aboominister@gmail.com>
2022-10-15 16:13:51 -0400

    Functionality for serving static files in the server

--- server/src/endpoints.rs
+++ server/src/endpoints.rs
@@ -1,6 +1,14 @@
-use axum::http::StatusCode;
+use axum::{http::StatusCode, response::Html};
+use std::include_str;
+
+static INDEX_CONTENT: &str = include_str!("../../public/index.html");
 
 // Health check endpoint
 pub async fn health_check() -> StatusCode {
     StatusCode::OK
+}
+
+// root endpoint
+pub async fn index() -> Html<&'static str> {
+    Html(INDEX_CONTENT)
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,16 +1,36 @@
 mod endpoints;
 
-use axum::{routing::get, Router};
-use endpoints::health_check;
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, get_service},
+    Router,
+};
+use axum_extra::routing::SpaRouter;
+use endpoints::{health_check, index};
+use std::io;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/health-check", get(health_check));
-    tracing_subscriber::fmt::init();
+    let serve_static = get_service(ServeDir::new("./static")).handle_error(handle_error);
+    let styles_router = SpaRouter::new("/styles", "./styles");
 
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/health-check", get(health_check))
+        .merge(styles_router)
+        .nest("/static", serve_static);
+
+    tracing_subscriber::fmt::init();
     tracing::info!("Listening on http://0.0.0.0:3000");
+
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn handle_error(_err: io::Error) -> impl IntoResponse {
+    (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong...")
 }
