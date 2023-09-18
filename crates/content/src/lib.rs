e7359a8c1bdd2136d87004f443cd2fb515611c17
AbooMinister25 <aboominister@gmail.com>
2023-09-18 17:14:11 -0400

    Added functionality to create tables in the database and discover posts to process

---
+++ crates/content/src/lib.rs
@@ -0,0 +1,78 @@
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
+use color_eyre::Result;
+use ignore::{DirEntry, Walk};
+use rusqlite::Connection;
+use sql::{get_hashes, insert_post, update_hash};
+use tracing::trace;
+
+#[derive(Debug)]
+pub struct Entry {
+    pub path: PathBuf,
+    pub content: String,
+    pub hash: String,
+}
+
+#[derive(Debug)]
+pub struct Post {
+    pub path: PathBuf,
+    pub content: String,
+}
+
+impl Post {
+    pub fn new(path: PathBuf, content: String) -> Self {
+        Self { path, content }
+    }
+}
+
+impl Entry {
+    pub fn from_file(path: PathBuf) -> Result<Self> {
+        let content = fs::read_to_string(&path)?;
+        let hash = format!("{:016x}", seahash::hash(content.as_bytes()));
+
+        Ok(Self {
+            path,
+            content,
+            hash,
+        })
+    }
+}
+
+/// Traverses the files in the current folder, reads in their contents, hashes them, and yields `Entry`s.
+#[tracing::instrument]
+pub fn discover_entries(path: &Path) -> Result<Vec<Entry>> {
+    let content_path = path.join("contents");
+    trace!("Discovering entries at {:?}", content_path);
+
+    Walk::new(content_path)
+        .filter_map(Result::ok)
+        .map(DirEntry::into_path)
+        .filter(|p| !p.is_dir())
+        .map(Entry::from_file)
+        .collect()
+}
+
+/// Filter entries to get only the ones that have changed, or have been newly created, since the last run of the program.
+#[tracing::instrument]
+pub fn to_build(conn: &Connection, entries: Vec<Entry>) -> Result<Vec<Entry>> {
+    let mut ret = Vec::new();
+
+    for entry in entries {
+        let hashes = get_hashes(conn, &entry.path)?;
+
+        if hashes.is_empty() {
+            // A new file was created.
+            insert_post(conn, &entry.path, &entry.hash)?;
+            ret.push(entry)
+        } else if hashes[0] != entry.hash {
+            // Existing file was changed.
+            update_hash(conn, &entry.path, &entry.hash)?;
+            ret.push(entry)
+        }
+    }
+
+    Ok(ret)
+}
---
+++ crates/sql/src/lib.rs
@@ -0,0 +1,74 @@
+use std::path::Path;
+
+use color_eyre::{eyre::ContextCompat, Result};
+use rusqlite::Connection;
+
+/// Sets up the SQLite database, creating the initial tables if they don't exist, and acquiring the connection.
+#[tracing::instrument]
+pub fn setup_sql() -> Result<Connection> {
+    // Establish connection to the database
+    let conn = Connection::open("blog.db")?;
+
+    conn.execute(
+        "
+        CREATE TABLE IF NOT EXISTS posts (
+            post_id INTEGER PRIMARY KEY,
+            path VARCHAR NOT NULL,
+            hash TEXT NOT NULL
+        )
+    ",
+        (),
+    )?;
+
+    Ok(conn)
+}
+
+// Fetch hash from database
+#[tracing::instrument]
+pub fn get_hashes(conn: &Connection, path: &Path) -> Result<Vec<String>> {
+    let mut stmt = conn.prepare("SELECT hash FROM posts WHERE path = :path")?;
+    let path_str = path
+        .to_str()
+        .context("Error while converting path to string")?;
+
+    // Get hashes found for the given path
+    let hashes_iter = stmt.query_map(&[(":path", path_str)], |row| row.get(0))?;
+    let mut hashes: Vec<String> = Vec::new();
+
+    for hash in hashes_iter {
+        hashes.push(hash?);
+    }
+
+    Ok(hashes)
+}
+
+// Insert a post into the database
+#[tracing::instrument]
+pub fn insert_post(conn: &Connection, path: &Path, hash: &str) -> Result<()> {
+    conn.execute(
+        "INSERT INTO posts (path, hash) VALUES (?1, ?2)",
+        (
+            &path
+                .to_str()
+                .context("Error while converting path to string")?,
+            &hash,
+        ),
+    )?;
+
+    Ok(())
+}
+
+// Update an existing post in the database with a new hash
+#[tracing::instrument]
+pub fn update_hash(conn: &Connection, path: &Path, new_hash: &str) -> Result<()> {
+    let mut stmt = conn.prepare("UPDATE posts SET hash = (:hash) WHERE path = (:path)")?;
+    stmt.execute(&[
+        (":hash", &new_hash),
+        (
+            ":path",
+            &path.to_str().context("Path should be valid unicode")?,
+        ),
+    ])?;
+
+    Ok(())
+}
