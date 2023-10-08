3b4da012935f60491645a6a13a253c08de6ee970
AbooMinister25 <aboominister@gmail.com>
2023-10-08 19:07:32 -0400

    Add rendering of posts and the tera templates that are required to do so

--- crates/content/src/lib.rs
+++ crates/content/src/lib.rs
@@ -1,19 +1,25 @@
 #![warn(clippy::pedantic, clippy::nursery)]
 #![allow(clippy::must_use_candidate)]
 #![allow(clippy::missing_const_for_fn)]
+#![allow(clippy::missing_errors_doc)]
 
 use std::{
     default::Default,
     fmt::Debug,
-    fs, io,
+    fs,
+    io::{self},
     path::{Path, PathBuf},
 };
 
 use color_eyre::Result;
 use ignore::{DirEntry, Walk};
+use markdown::MarkdownRenderer;
 use rusqlite::Connection;
 use sql::{get_hashes, insert_post, update_hash};
+use tera::{Context, Tera};
 use tracing::trace;
+
+pub const DATE_FORMAT: &str = "%b %e, %Y";
 
 #[derive(Debug)]
 pub struct Entry {
@@ -37,6 +43,35 @@ impl Post {
             ..Default::default()
         }
     }
+
+    pub fn render<T: AsRef<Path>>(
+        &mut self,
+        tera: &Tera,
+        renderer: &MarkdownRenderer,
+        output_directory: T,
+    ) -> Result<()> {
+        ensure_directory(output_directory.as_ref().join("posts/"))?;
+        let document = renderer.render(&self.raw_content)?;
+
+        let out_path = output_directory
+            .as_ref()
+            .join("posts/")
+            .join(format!("{}.html", document.frontmatter.title));
+
+        let mut context = Context::new();
+        context.insert("title", &document.frontmatter.title);
+        context.insert("tags", &document.frontmatter.tags.join(", "));
+        context.insert("date", &document.date.format(DATE_FORMAT).to_string());
+        context.insert("toc", &document.toc);
+        context.insert("markup", &document.content);
+        context.insert("summary", &document.summary);
+
+        let rendered_html = tera.render("post.html.tera", &context)?;
+        fs::write(out_path, &rendered_html)?;
+        self.content = rendered_html;
+
+        Ok(())
+    }
 }
 
 impl Entry {
@@ -55,8 +90,8 @@ impl From<Entry> for Post {
     }
 }
 
-/// Traverse files in content folder, read in their content, hash them, and then filter to get
-/// only the ones that have changed, or have been newly created, since the last run of the program.
+/// Traverse files in content folder, read them, hash them, and then filter out only the
+/// ones that have changed or have been newly created since the last run of the program.
 #[tracing::instrument]
 pub fn discover_entries<P: AsRef<Path> + Debug>(conn: &Connection, path: P) -> Result<Vec<Entry>> {
     let mut ret = Vec::new();
@@ -97,4 +132,13 @@ pub fn discover_entries<P: AsRef<Path> + Debug>(conn: &Connection, path: P) -> R
     }
 
     Ok(ret)
+}
+
+// If the given directory doesn't exist, creates it.
+pub fn ensure_directory<T: AsRef<Path>>(path: T) -> Result<()> {
+    if !path.as_ref().exists() {
+        fs::create_dir_all(path)?;
+    }
+
+    Ok(())
 }
--- crates/site/src/lib.rs
+++ crates/site/src/lib.rs
@@ -8,18 +8,22 @@ use color_eyre::Result;
 use content::Post;
 use markdown::MarkdownRenderer;
 use rusqlite::Connection;
+use tera::Tera;
+use tracing::info;
 
 #[derive(Debug)]
 pub struct Context {
     pub conn: Connection,
     pub markdown_renderer: MarkdownRenderer,
+    pub tera: Tera,
 }
 
 impl Context {
-    pub fn new(conn: Connection, markdown_renderer: MarkdownRenderer) -> Self {
+    pub fn new(conn: Connection, markdown_renderer: MarkdownRenderer, tera: Tera) -> Self {
         Self {
             conn,
             markdown_renderer,
+            tera,
         }
     }
 }
@@ -33,18 +37,29 @@ pub struct Site {
 }
 
 impl Site {
+    #[tracing::instrument]
     pub fn new<P: AsRef<Path> + Debug>(conn: Connection, path: P) -> Result<Self> {
         let entries = content::discover_entries(&conn, &path)?;
         let posts = entries.into_iter().map(Post::from).collect::<Vec<Post>>();
 
         let renderer = MarkdownRenderer::new()?;
-
-        let ctx = Context::new(conn, renderer);
+        info!("Loaded templates");
+        let tera = Tera::new("templates/**/*.tera")?;
+        let ctx = Context::new(conn, renderer, tera);
 
         Ok(Self {
             ctx,
             site_path: path.as_ref().to_path_buf(),
             posts,
         })
+    }
+
+    #[tracing::instrument]
+    pub fn build(&mut self) -> Result<()> {
+        for post in &mut self.posts {
+            post.render(&self.ctx.tera, &self.ctx.markdown_renderer, "public/")?;
+        }
+
+        Ok(())
     }
 }
