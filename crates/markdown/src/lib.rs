74375c11301b5e84dd949e1e53445671fa054b60
AbooMinister25 <aboominister@gmail.com>
2024-07-18 15:00:05 -0400

    Add tests for markdown

--- crates/markdown/src/lib.rs
+++ crates/markdown/src/lib.rs
@@ -20,7 +20,6 @@ pub struct Frontmatter {
     pub title: String,
     pub tags: Vec<String>,
     pub template: Option<String>,
-    pub completed: Option<bool>,
     pub date: Option<String>,
     pub updated: Option<String>,
     pub series: Option<SeriesInfo>,
@@ -56,7 +55,8 @@ impl<'c> MarkdownRenderer<'c> {
     pub fn new<P: AsRef<Path> + Debug>(path: P, theme: &str) -> Result<Self> {
         // Load the theme set
         let ss = SyntaxSet::load_defaults_newlines();
-        let theme_set = ThemeSet::load_from_folder(path.as_ref().join("themes/"))?;
+        let mut theme_set = ThemeSet::load_defaults();
+        theme_set.add_from_folder(path.as_ref().join("themes/"))?;
 
         // Create an adapter and choose the syntax highlighting theme.
         let adapter = SyntectAdapterBuilder::new()
@@ -177,5 +177,236 @@ impl<'c> MarkdownRenderer<'c> {
 
         let frontmatter = toml::from_str(&frontmatter_content)?;
         Ok(frontmatter)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::create_dir;
+    use tempfile::tempdir;
+
+    fn get_renderer<'a>() -> Result<MarkdownRenderer<'a>> {
+        let tmp_dir = tempdir()?;
+        create_dir(tmp_dir.path().join("themes/"))?;
+        let renderer = MarkdownRenderer::new(tmp_dir.path(), "Solarized (light)")?;
+
+        Ok(renderer)
+    }
+
+    #[test]
+    fn test_parse_markdown() -> Result<()> {
+        let content = r#"---
+title = "Test"
+tags = ["a", "b", "c"]
+---
+
+Hello World
+"#;
+        let renderer = get_renderer()?;
+        let document = renderer.render(content)?;
+
+        assert_eq!(document.frontmatter.title, "Test".to_string());
+        assert_eq!(
+            document.frontmatter.tags,
+            vec!["a".to_string(), "b".to_string(), "c".to_string()]
+        );
+        assert_eq!(document.content, "<p>Hello World</p>\n".to_string());
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_summary() -> Result<()> {
+        let content = r#"---
+title = "Test"
+tags = ["a", "b", "c"]
+---
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Suspendisse ut mattis felis. Mauris sed ex vitae est pharetra 
+scelerisque. Ut ut sem arcu. Morbi molestie dictum venenatis. 
+Quisque sit amet consequat libero. Cras id tellus diam. Cras 
+pulvinar tristique nisl vel porttitor. Fusce enim magna, porta 
+sed nisl non, dignissim ultrices massa. Sed ultrices tempus dolor sit 
+amet fringilla. Proin at mauris porta, efficitur magna sit amet, 
+rutrum elit. In efficitur vitae erat id scelerisque. Cras laoreet 
+elit eu neque condimentum auctor. Lorem ipsum dolor sit amet, 
+consectetur adipiscing elit. Vivamus nec auctor neque, at 
+consectetur velit. Maecenas at massa ante.
+        "#;
+        let renderer = get_renderer()?;
+        let document = renderer.render(content)?;
+
+        assert_eq!(document.summary, "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nSuspendisse ut mattis felis. Mauris sed ex vitae est pharetra\nscelerisque. Ut ut sem arcu. Morbi molestie dictum venenatis.\nQuisque sit amet consequat libero. Cras id tellus diam. Cras\npulvinar tristique nisl vel porttitor. Fusce enim magna, porta\nsed nisl non, dignissim ultrices massa. Sed ultrices tempus dolor sit\namet fringilla. Proin at mauris porta, efficitur magna sit amet,\nrutrum elit. In efficitur vitae erat id scelerisque. Cras laoreet\nelit eu neque condimentum auctor. Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit. Vivamus nec auctor neque, at\nconsectetur velit. Maecenas at massa ante.</p>\n".to_string());
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_template() -> Result<()> {
+        let content = r#"---
+title = "Test"
+tags = ["a", "b", "c"]
+template = "index.html.tera"
+---
+
+Hello World
+"#;
+        let renderer = get_renderer()?;
+        let document = renderer.render(content)?;
+
+        assert_eq!(
+            document.frontmatter.template,
+            Some("index.html.tera".to_string())
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_date() -> Result<()> {
+        let content = r#"---
+title = "Test"
+tags = ["a", "b", "c"]
+date = "2024-07-17T6:00:00"
+---
+
+Hello World
+"#;
+        let renderer = get_renderer()?;
+        let document = renderer.render(content)?;
+
+        assert_eq!(
+            document.date,
+            Utc.from_utc_datetime(&"2024-07-17T6:00:00".parse::<NaiveDateTime>()?)
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_updated() -> Result<()> {
+        let content = r#"---
+title = "Test"
+tags = ["a", "b", "c"]
+date = "2024-07-12T6:00:00"
+updated = "2024-07-17T6:00:00"
+---
+
+Hello World
+"#;
+        let renderer = get_renderer()?;
+        let document = renderer.render(content)?;
+
+        assert_eq!(
+            document.updated,
+            Utc.from_utc_datetime(&"2024-07-17T6:00:00".parse::<NaiveDateTime>()?)
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_updated_default() -> Result<()> {
+        let content = r#"---
+title = "Test"
+tags = ["a", "b", "c"]
+date = "2024-07-12T6:00:00"
+---
+
+Hello World
+"#;
+        let renderer = get_renderer()?;
+        let document = renderer.render(content)?;
+
+        assert_eq!(
+            document.updated,
+            Utc.from_utc_datetime(&"2024-07-12T6:00:00".parse::<NaiveDateTime>()?)
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_slug() -> Result<()> {
+        let content = r#"---
+title = "Test"
+tags = ["a", "b", "c"]
+slug = "test"
+---
+
+Hello World
+"#;
+        let renderer = get_renderer()?;
+        let document = renderer.render(content)?;
+
+        assert_eq!(document.frontmatter.slug, Some("test".to_string()));
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_draft() -> Result<()> {
+        let content = r#"---
+title = "Test"
+tags = ["a", "b", "c"]
+---
+
+Hello World
+"#;
+        let renderer = get_renderer()?;
+        let document = renderer.render(content)?;
+
+        assert!(!document.frontmatter.draft);
+
+        let content = r#"---
+title = "Test"
+tags = ["a", "b", "c"]
+draft = true
+---
+
+Hello World
+"#;
+        let document = renderer.render(content)?;
+        assert!(document.frontmatter.draft);
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_series() -> Result<()> {
+        let content = r#"---
+title = "Test"
+tags = ["a", "b", "c"]
+
+[series]
+part = 1
+---
+
+Hello World
+"#;
+        let renderer = get_renderer()?;
+        let document = renderer.render(content)?;
+
+        assert_eq!(document.frontmatter.series.unwrap().part, Some(1));
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_toc() -> Result<()> {
+        let content = r#"---
+title = "Test"
+tags = ["a", "b", "c"]
+---
+
+## First
+hi
+
+## Second
+hi
+
+## Third
+hi
+"#;
+        let renderer = get_renderer()?;
+        let document = renderer.render(content)?;
+
+        assert_eq!(document.toc, vec!["First", "Second", "Third"]);
+        Ok(())
     }
 }
