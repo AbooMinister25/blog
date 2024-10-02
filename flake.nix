35f29b14a52ae3b794691237924baa6968b323f4
AbooMinister25 <aboominister@gmail.com>
2024-10-01 22:21:48 -0400

    Add flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A basic flake with a shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs ={ self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem
+    (system:
+      let
+        overlays = [ ( import rust-overlay )];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [ rust-bin.stable.latest.default ];
+        };
+      }
+    );
+}
